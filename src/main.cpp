3b34cfe1864d9058079225b5685e1b11a28c1d5c
INTechLucas <lucas.molia@telecom-sudparis.eu>
2017-11-23 18:44:34 +0100

    Premier commit pushi pusha

---
+++ src/main.cpp
@@ -0,0 +1,31 @@
+#include "Arduino.h"
+#include <Actuators/DynamixelInterface.h>
+#include <Actuators/DynamixelMotor.h>
+
+HardwareSerial serie;
+DynamixelInterface interface(serie,1); //S'ouvre sur le port 1
+
+DynamixelMotor moteur(interface,254); //Ouvre une communication avec tout les AX12 - Remplacer 254 par l'ID
+
+//Initialisation de la Serie
+void setup() {
+	/*serie*/
+
+
+	Serial.begin(115200);
+	Serial.println("Série OK");
+	delay(250);
+	
+	
+	interface.begin(9600);  //Ouvre l'interface
+
+	moteur.init();			//Initialise le moteur
+	moteur.enableTorque();	//Et active le couple
+	moteur.jointMode();
+}
+
+//Boucle principale
+void loop() {
+	Serial.println("Ca tourne!");
+	delay(500);
+}
