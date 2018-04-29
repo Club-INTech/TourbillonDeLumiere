118262acf084d7c2af3a1c27ef8ef6488b24eb82
INTechLucas <lucas.molia@telecom-sudparis.eu>
2018-04-29 16:48:20 +0200

    On vire le code de test et on remet le reste pour faire un match normal

--- src/main.cpp
+++ src/main.cpp
@@ -30,9 +30,9 @@ void loop() {
     while(!robot.start()) { //on attend le debut du match , front montant
         delay(100);
     }
-
+    
     /*Début du match, on active les interruptions et du timer pour l'arret automatique */
-    /*attachInterrupt(digitalPinToInterrupt(PIN_FIN_COURSE), comeBackUnderLoader, FALLING);
+    attachInterrupt(digitalPinToInterrupt(PIN_FIN_COURSE), comeBackUnderLoader, FALLING);
     attachInterrupt(digitalPinToInterrupt(PIN_LASER), checkLoadedFired, CHANGE);
     timer.begin(stopMatch, 100000000);
 
@@ -55,12 +55,7 @@ void loop() {
         robot.addScore(5);
     }
 
-    stopMatch();*/
-
-    SoftPWMSet(PIN_TURBINE, PWM_TURBINE_GREEN);
-    delay(1000);
-    SoftPWMSet(PIN_TURBINE,0);
-    delay(1000);
+    stopMatch();
 }
 
 void comeBackUnderLoader(){
